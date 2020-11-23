650d84b1eecb6fef10f48df569e127b39fda83e6
Raymond Aina <rta2@hw.ac.uk>
2020-11-23 14:13:45 +0000

    Initial Commit

---
+++ SaveTheKittens/SaveTheKittensApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveTheKittensApp.swift
+//  SaveTheKittens
+//
+//  Created by Raymond Aina on 23/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SaveTheKittensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
